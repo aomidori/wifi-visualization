09688f91490153e881de63e0a475fc0a2fca9710
Hanyue Zhou <hanyue.zhou@volumental.com>
2024-01-08 05:03:50 +0100

    split vender chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    splitVendorChunkPlugin(),
+  ],
   resolve: {
     alias: {
       '#': '/src',
